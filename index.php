92ef7c6ba6c5c5a05bfd163ea00645106a1ee63b
Schwiiiii <brett.schwickerath@gmail.com>
2019-03-03 20:18:56 -0600

    making sass visible and updating look and feel

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("includes/pgtop.php"); ?>
+
+<div class="d-flex flex-row justify-content-start align-items-center p-3 bg-dark-primary">
+    <div>
+        <svg class="svg-logo mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 144 144">
+            <path fill="#000" d="M50.79 3.18A71.35 71.35 0 0 0 36 9.66 71.85 71.85 0 0 0 0 72a72.23 72.23 0 0 0 36 62.34l.62.36a71.95 71.95 0 0 0 70.76 0A72 72 0 0 0 36 72 36 36 0 0 0 50.79 3.18z"/>
+            <path fill="#000" d="M140.82 50.79A72.17 72.17 0 0 0 108 9.66a71.35 71.35 0 0 0-14.79-6.48A36 36 0 0 0 108 72a71.65 71.65 0 0 1 35.38 9.3 71.63 71.63 0 0 0-2.56-30.51z"/>
+        </svg>
+    </div>
+    <div>
+        <h6 class="text-primary">Brett Schwickerath</h6>
+    </div>
+</div>
+
+<?php include("includes/pgbot.php"); ?>
